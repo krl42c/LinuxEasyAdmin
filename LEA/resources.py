5cee10c1f76f790cce1a191f6326f3124be6d21d
Karol Ciechlecki <karolc9991@gmail.com>
2020-09-29 23:50:08 +0200

    Added new methods, testing html

--- LEA/resources.py
+++ LEA/resources.py
@@ -1,6 +1,41 @@
-import psutil
+import psutil,sys
 
-def getcpu():
-    return 5
+
+#Returns the current platform the program is running in
+def getOS():
+    return str(sys.platform)
+
+#Returns a list with the current processes that are running
+#TODO: change list to dictionary, add PID to be able to make operations with them
+def getProcessList():
+    processList = []
+
+    for p in psutil.process_iter():
+        pName = p.name()
+        processList.append(str(pName))
+
+    return processList;
+
+#Receives a disk as "/dev/hdx" and returns it currents space, if no argument is passed
+#returns the whole disk size
+def getDiskSpace(disk=None):
+    hd_space = None
+    if disk is None:
+        hd_space = psutil.disk_usage('/')
+    else:
+        hd_space = psutil.disk_usage(disk)
+
+    return hd_space
+
+#Returns the percentage of the battery
+def getBatteryPercentage():
+    battery = psutil.sensors_battery()
+    return str(battery.percent)
+
+def getRAMUsagePercent():
+    return psutil.virtual_memory().percent
+
+def getRAM():
+    return psutil.virtual_memory()
 
 
--- LEA/views.py
+++ LEA/views.py
@@ -3,6 +3,10 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template('base.html',title='INICIO')
+    return render_template('base.html',title='INICIO',processList=resources.getProcessList(), mem=resources.getMem(),os=resources.getOS())
 
+
+@app.route('/ram')
+def ram():
+    return render_template('ram.html',ram_percent=resources.getRAMUsagePercent())
 
